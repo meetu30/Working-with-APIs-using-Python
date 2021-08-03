8c671a5c66904c745ff5066c8c0fa02f85b7c0a2
meetu30 <34873916+meetu30@users.noreply.github.com>
2021-08-03 11:51:22 -0400

    Update AdobeAnalyticsAPI2.0_Level1.py

--- AdobeAnalyticsAPI2.0_Level1.py
+++ AdobeAnalyticsAPI2.0_Level1.py
@@ -1,53 +1,4 @@
-import time
-from datetime import datetime,timedelta
-import requests, yaml
-import re
-import sys 
-import pyodbc
-from urllib.parse import quote
-import urllib3 
-urllib3.disable_warnings()
-yaml.warnings({ 'YAML Loadwarning': False})
-import json
 
-################ Create a log file for today ##############
-tnow = str(datetime.now() - timedelta(0)).split()
-datetimenow = tnow[0] + '_' + '(' + tnow[1].split('.')[0] + ')'
-datetimenow = datetimenow.replace(':', '_')
-log = open("log_" + datetimenow + ".txt", "a+")
-
-############### Load config file to capture all credentials ############
-try:
-    with open("config.yml", "r") as dbconf:
-        dbconfig = yaml.load(dbconf, Loader = yaml.FullLoader)
-        
-    UID = dbconfig.get("UID")
-    PWD = dbconfig.get("PWD")
-    DataBase = dbconfig.get("DATABASE")
-    Server = dbconfig.get("SERVER")
-    
-    td_var = dbconfig.get("TD_VAR")
-    accessToken = dbconfig.get("ACCTOK")
-    apiKey = dbconfig.get("apiKey")
-    orgID =  dbconfig.get("orgID")
-    globalCompanyID = dbconfig.get("globalCompanyId")
-    rsID = dbconfig.get("rsID")
-    segmentID = dbconfig.get("segmentID")
-    segmentName = dbconfig.get("segmentName")
-    
-
-    log.write("Credentials loaded successfully \n")  
-except Exception as e:
-    log.write("Error occured while loading DB credentials")
-    
-################# fetch today's and yesterday's time #############
-tyesterday = str(datetime.now() - timedelta(td_var + 1)).split(" ")
-today = str(datetime.now() - timedelta(td_var)).split(" ")[0]
-yesterday = str(datetime.now() - timedelta(td_var + 1)).split(" ")[0]
-
-############### get time range in API format #################
-range_from = tyesterday[0] + "T00:00:00.000"
-range_to = today[0] + "T00:00:00.000"
 
 ############## Define header with all credentials ###########
 header = {
@@ -118,49 +69,5 @@ for d in response['rows']:
         
                         ])
 
-############## Set up connection to database ##############
-try:
-    connection = pyodbc.connect("Driver = {ODBC Driver 13 for SQL Server};"
-                                "Server =" +Server+";"
-                                "Database = " +DataBase+ ";"
-                                "uid = " +UID+ ";"
-                                "password = " +PWD,char_encoding = 'utf-8',
-                                wchar_encoding = "utf-161e")
-    
-    cursor = connection.cursor()
-    log.write("DB connection Successful\n")
-    
-except Exception as e:
-    log.write("Error occurred while connecting to backend DB: " + str(e))
 
-
-############## Inserting data to the backend table ##############
-try:
-    
-    ##### Check if the data exists for the given date
-    cursor.execute("SELECT date FROM GIVE_TABLE_NAME WHERE data = ?", yesterday)
-    data_lookup = []
-    for record in cursor.fetchall():
-        data_lookup.append(str(record))
-        
-    ##### if data exists then come out of the script, else insert data    
-    if data_lookup:
-        log.write("Data already exists in the table!!")
-        sys.exit(0)
-    
-    #### Insert data to the given table
-    cursor.executemany("INSERT INTO GIVE_TABLE_NAME(segmentID, page, visits, uniqueVisitors, revenue, bouncerate, date) VALUES (?,?,?,?,?,?,?);", page_details)
-    log.write("Data inserted to GIVE_TABLE_NAME table successfully!")
-
-except Exception as e:
-    log.write("Error occurred while pushing data to DB: " + str(e))
-    
-connection.commit()
-connection.close()
-log.close()
-
-###################### End of file #####################
-
-        
-    
 
